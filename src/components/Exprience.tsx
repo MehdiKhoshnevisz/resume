b265d93ff104c0c293d4388c56ecbd89a8fda1d3
mehdi.khoshnevis <mehdi.khoshnevis@buzzjective.com>
2022-05-23 14:06:11 +0430

    Create Exprienct component

---
+++ src/components/Exprience.tsx
@@ -0,0 +1,65 @@
+type Props = {
+  company: string;
+  position: string;
+  startDate: string;
+  endDate?: string;
+  details?: string[];
+};
+
+const Company = ({ title }: any) => (
+  <h3 style={{ color: "#333", marginBottom: ".25rem" }}>{title}</h3>
+);
+
+const Position = ({ title, startDate, endDate }: any) => {
+  return (
+    <span
+      style={{
+        display: "block",
+        fontSize: "1.17rem",
+        color: "#777",
+        marginBottom: ".25rem",
+      }}
+    >
+      {title} &nbsp; <span style={{ fontSize: "1rem" }}>|</span> &nbsp;{" "}
+      {startDate} - {endDate || "Present"}
+    </span>
+  );
+};
+
+const Details = ({ details }: any) => {
+  return (
+    <ul
+      style={{
+        fontSize: "1.17rem",
+        listStyle: "none",
+        padding: 0,
+        margin: 0,
+        color: "#333",
+      }}
+    >
+      {details.map((item: string, index: number) => (
+        <li key={index} style={{ marginBottom: "0.25rem" }}>
+          - {item}
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default function Exprience({
+  company,
+  position,
+  startDate,
+  endDate,
+  details,
+}: Props) {
+  return (
+    <div>
+      <Company title={company} />
+
+      <Position title={position} startDate={startDate} endDate={endDate} />
+
+      {details && <Details details={details} />}
+    </div>
+  );
+}
