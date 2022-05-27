036aad28bf2bec94ac8ac4359155940a00a903fe
mehdi.khoshnevis <mehdi.khoshnevis@buzzjective.com>
2022-05-27 20:28:55 +0430

    Create Education component

---
+++ src/components/Education.tsx
@@ -0,0 +1,35 @@
+type Props = {
+  title: string;
+  field: string;
+  startDate: string;
+  endDate?: string;
+};
+
+const Title = ({ title }: any) => (
+  <h3 style={{ color: "#333", marginBottom: ".25rem" }}>{title}</h3>
+);
+
+const Field = ({ field, startDate, endDate }: any) => {
+  return (
+    <span
+      style={{
+        display: "block",
+        fontSize: "1.17rem",
+        color: "#777",
+        marginBottom: ".25rem",
+      }}
+    >
+      {field} <br />
+      {startDate} - {endDate || "Present"}
+    </span>
+  );
+};
+
+export default function Education({ title, field, startDate, endDate }: Props) {
+  return (
+    <>
+      <Title title={title} />
+      <Field field={field} startDate={startDate} endDate={endDate} />
+    </>
+  );
+}
