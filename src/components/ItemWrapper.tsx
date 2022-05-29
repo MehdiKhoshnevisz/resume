25100da690e823de9fc870ad9c9f59593720063e
MehdiKhoshnevisz <mkhnsz1996@gmail.com>
2022-05-29 16:51:05 +0430

    Add padding in ItemWrapper component

--- src/components/ItemWrapper.tsx
+++ src/components/ItemWrapper.tsx
@@ -8,8 +8,10 @@ type Props = {
 export default function ItemWrapper({ title, children }: Props) {
   return (
     <div className="mk-resume-data__item">
-      <h2 style={{ color: "#777", margin: 0 }}>{title}</h2>
-      {children}
+      <div style={{ padding: "1rem" }}>
+        <h2 style={{ color: "#777", margin: 0 }}>{title}</h2>
+        {children}
+      </div>
     </div>
   );
 }
