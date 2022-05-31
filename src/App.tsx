a35f333c0800138abd213f71889fbfe91ad9f095
MehdiKhoshnevisz <mkhnsz1996@gmail.com>
2022-05-31 12:50:30 +0430

    Remove useless div element

--- src/App.tsx
+++ src/App.tsx
@@ -21,73 +21,71 @@ function App() {
 
       <hr style={{ margin: "3rem 0", opacity: 0.3 }} />
 
-      <div>
-        <div className="mk-resume-data">
-          <ItemWrapper title="Exprience">
-            <Exprience
-              company="Apple Company"
-              position="Technical lead"
-              startDate="2021"
-              details={[
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam euismod, nisi vitae tincidunt congue, nisl nunc egestas nisi, euismod aliquet nisl nunc eget.",
-              ]}
-            />
+      <div className="mk-resume-data">
+        <ItemWrapper title="Exprience">
+          <Exprience
+            company="Apple Company"
+            position="Technical lead"
+            startDate="2021"
+            details={[
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam euismod, nisi vitae tincidunt congue, nisl nunc egestas nisi, euismod aliquet nisl nunc eget.",
+            ]}
+          />
 
-            <Exprience
-              company="Google"
-              position="Senior Software Developer"
-              startDate="2018"
-              endDate="2020"
-              details={[
-                "Started and scaled design team to 6 product designers",
-                "Implemented a web application desgin System",
-              ]}
-            />
+          <Exprience
+            company="Google"
+            position="Senior Software Developer"
+            startDate="2018"
+            endDate="2020"
+            details={[
+              "Started and scaled design team to 6 product designers",
+              "Implemented a web application desgin System",
+            ]}
+          />
 
-            <Exprience
-              company="Buzzjective Company"
-              position="Software Developer"
-              startDate="2016"
-              endDate="2018"
-              details={[
-                "Started and scaled design team to 6 product designers",
-                "Implemented a web application desgin System",
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-              ]}
-            />
-          </ItemWrapper>
+          <Exprience
+            company="Buzzjective Company"
+            position="Software Developer"
+            startDate="2016"
+            endDate="2018"
+            details={[
+              "Started and scaled design team to 6 product designers",
+              "Implemented a web application desgin System",
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+            ]}
+          />
+        </ItemWrapper>
 
-          <ItemWrapper title="Education">
-            <Education
-              title="Sharif University"
-              field="Computer Science &nbsp; - &nbsp; Software Engineering"
-              startDate="2018"
-              endDate="2022"
-            />
+        <ItemWrapper title="Education">
+          <Education
+            title="Sharif University"
+            field="Computer Science &nbsp; - &nbsp; Software Engineering"
+            startDate="2018"
+            endDate="2022"
+          />
 
-            <Education
-              title="Shahab Danesh University"
-              field="Computer Engineer &nbsp; - &nbsp; Information Technology"
-              startDate="2014"
-              endDate="2018"
-            />
-          </ItemWrapper>
+          <Education
+            title="Shahab Danesh University"
+            field="Computer Engineer &nbsp; - &nbsp; Information Technology"
+            startDate="2014"
+            endDate="2018"
+          />
+        </ItemWrapper>
 
-          <ItemWrapper title="Exprience">
-            <Exprience
-              company="Apple Company"
-              position="Technical lead"
-              startDate="2021"
-              details={[
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam euismod, nisi vitae tincidunt congue, nisl nunc egestas nisi, euismod aliquet nisl nunc eget.",
-              ]}
-            />
-          </ItemWrapper>
+        <ItemWrapper title="Exprience">
+          <Exprience
+            company="Apple Company"
+            position="Technical lead"
+            startDate="2021"
+            details={[
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam euismod, nisi vitae tincidunt congue, nisl nunc egestas nisi, euismod aliquet nisl nunc eget.",
+            ]}
+          />
+        </ItemWrapper>
 
-          <ItemWrapper title="Achievement">
-            <Achievement description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam euismod, nisi vitae tincidunt congue, nisl nunc egestas nisi, euismod aliquet nisl nunc eget." />
-          </ItemWrapper>
-        </div>
+        <ItemWrapper title="Achievement">
+          <Achievement description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam euismod, nisi vitae tincidunt congue, nisl nunc egestas nisi, euismod aliquet nisl nunc eget." />
+        </ItemWrapper>
       </div>
     </main>
   );
