17aefb993ea16b00fd733a2d660f0e9782553082
MehdiKhoshnevisz <mkhnsz1996@gmail.com>
2022-06-06 13:15:03 +0430

    Import Footer in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import Footer from "./components/Footer";
 import Skills from "./components/Skills";
 import Project from "./components/Project";
 import Biography from "./components/Biography";
@@ -10,96 +11,100 @@ import LineSeparator from "./components/LineSeparator";
 
 function App() {
   return (
-    <main id="app">
-      <Biography
-        firstName="Mehdi"
-        lastName="Khoshnevis"
-        title="Senior Software Developer"
-        website="https://imehdi.dev"
-        description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque voluptate
+    <>
+      <main id="app">
+        <Biography
+          firstName="Mehdi"
+          lastName="Khoshnevis"
+          title="Senior Software Developer"
+          website="https://imehdi.dev"
+          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque voluptate
         repellat ipsam repudiandae explicabo ratione aliquid, quo maxime ab unde
         distinctio nihil quod iure at ad, tempora odit mollitia. Amet repellat
         ipsam repudiandae explicabo ratione aliquid, quo maxime ab unde
         distinctio nihil quod iure at ad, tempora odit mollitia. Amet."
-      />
-
-      <LineSeparator />
-
-      <ItemWrapper title="Skills">
-        <Skills
-          list={["HTML5", "CSS3", "Javascript", "Vuejs", "Reactjs", "Nodejs"]}
-        />
-      </ItemWrapper>
-
-      <LineSeparator />
-
-      <ItemWrapper title="Work Exprience">
-        <Exprience
-          company="Apple Company"
-          position="Technical lead"
-          startDate="2021"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam euismod, nisi vitae tincidunt congue, nisl nunc egestas nisi, euismod aliquet nisl nunc eget."
         />
 
-        <Exprience
-          company="Google"
-          position="Senior Software Developer"
-          startDate="2018"
-          endDate="2020"
-          description="Started and scaled design team to 6 product designers and Implemented a web application desgin System"
-        />
+        <LineSeparator />
 
-        <Exprience
-          company="Buzzjective Company"
-          position="Software Developer"
-          startDate="2016"
-          endDate="2018"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam euismod, nisi vitae tincidunf."
-        />
-      </ItemWrapper>
+        <ItemWrapper title="Skills">
+          <Skills
+            list={["HTML5", "CSS3", "Javascript", "Vuejs", "Reactjs", "Nodejs"]}
+          />
+        </ItemWrapper>
 
-      <LineSeparator />
+        <LineSeparator />
 
-      <ItemWrapper title="Education">
-        <Education
-          title="Sharif University"
-          field="Computer Science &nbsp; - &nbsp; Software Engineering"
-          startDate="2018"
-          endDate="2022"
-        />
+        <ItemWrapper title="Work Exprience">
+          <Exprience
+            company="Apple Company"
+            position="Technical lead"
+            startDate="2021"
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam euismod, nisi vitae tincidunt congue, nisl nunc egestas nisi, euismod aliquet nisl nunc eget."
+          />
 
-        <Education
-          title="Shahab Danesh University"
-          field="Computer Engineer &nbsp; - &nbsp; Information Technology"
-          startDate="2014"
-          endDate="2018"
-        />
-      </ItemWrapper>
+          <Exprience
+            company="Google"
+            position="Senior Software Developer"
+            startDate="2018"
+            endDate="2020"
+            description="Started and scaled design team to 6 product designers and Implemented a web application desgin System"
+          />
 
-      <LineSeparator />
+          <Exprience
+            company="Buzzjective Company"
+            position="Software Developer"
+            startDate="2016"
+            endDate="2018"
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam euismod, nisi vitae tincidunf."
+          />
+        </ItemWrapper>
 
-      <ItemWrapper title="Achievement">
-        <Achievement description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam euismod, nisi vitae tincidunt congue, nisl nunc egestas nisi, euismod aliquet nisl nunc eget." />
-      </ItemWrapper>
+        <LineSeparator />
 
-      <LineSeparator />
+        <ItemWrapper title="Education">
+          <Education
+            title="Sharif University"
+            field="Computer Science &nbsp; - &nbsp; Software Engineering"
+            startDate="2018"
+            endDate="2022"
+          />
 
-      <ItemWrapper title="Projects">
-        <Project
-          title="Project 1"
-          startDate="2020"
-          endDate="2022"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam euismod, nisi vitae tincidunt congue, nisl nunc egestas nisi, euismod aliquet nisl nunc eget."
-        />
+          <Education
+            title="Shahab Danesh University"
+            field="Computer Engineer &nbsp; - &nbsp; Information Technology"
+            startDate="2014"
+            endDate="2018"
+          />
+        </ItemWrapper>
 
-        <Project
-          title="Project 2"
-          startDate="2020"
-          endDate="2022"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam euismod, nisi vitae tincidunt congue, nisl nunc egestas nisi, euismod aliquet nisl nunc eget."
-        />
-      </ItemWrapper>
-    </main>
+        <LineSeparator />
+
+        <ItemWrapper title="Achievement">
+          <Achievement description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam euismod, nisi vitae tincidunt congue, nisl nunc egestas nisi, euismod aliquet nisl nunc eget." />
+        </ItemWrapper>
+
+        <LineSeparator />
+
+        <ItemWrapper title="Projects">
+          <Project
+            title="Project 1"
+            startDate="2020"
+            endDate="2022"
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam euismod, nisi vitae tincidunt congue, nisl nunc egestas nisi, euismod aliquet nisl nunc eget."
+          />
+
+          <Project
+            title="Project 2"
+            startDate="2020"
+            endDate="2022"
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam euismod, nisi vitae tincidunt congue, nisl nunc egestas nisi, euismod aliquet nisl nunc eget."
+          />
+        </ItemWrapper>
+      </main>
+
+      <Footer />
+    </>
   );
 }
 
