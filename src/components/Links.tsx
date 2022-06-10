90d182305589f8f3392cf3525da9f842dfb470c6
MehdiKhoshnevisz <mkhnsz1996@gmail.com>
2022-06-10 04:53:41 +0430

    Create Links component

--- src/components/Links.tsx
+++ src/components/Links.tsx
@@ -1,20 +1,36 @@
-import cleanURL from "../utils/cleanURL";
+import { useState, useEffect } from "react";
+import { cleanURL, getIcon, originURL } from "../utils";
 
 type Props = {
   items: object[];
 };
 
 export default function Contact({ items }: Props) {
-  return (
-    <>
-      {items.map((item: any) => (
-        <div>
-          <h3 className="mk-general-item-title">{item.title}</h3>
-          <a href={item.url} className="mk-general-item-sub-title">
+  const linkList: any = [];
+  const [links, setLinks] = useState(linkList);
+
+  useEffect(() => {
+    items.forEach((item: any, index) => {
+      const icon = getIcon(originURL(item.url));
+
+      const listItem = (
+        <div className="mk-link-list" key={index}>
+          <img src={icon} className="mk-link-list__icon" alt=" " />
+          <h3 className="mk-general-item-title" style={{ marginRight: ".3em" }}>
+            {item.title}
+          </h3>
+          <a
+            href={item.url}
+            className="mk-general-item-sub-title mk-link-list__url "
+          >
             {cleanURL(item.url)}
           </a>
         </div>
-      ))}
-    </>
-  );
+      );
+
+      setLinks((oldArray: any) => [...oldArray, listItem]);
+    });
+  }, [items]);
+
+  return <>{links}</>;
 }
