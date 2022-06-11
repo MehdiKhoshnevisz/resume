ef6dfd3a88dc9b84ee4a076ad47d2c8a3105e4ab
MehdiKhoshnevisz <mkhnsz1996@gmail.com>
2022-06-11 12:05:43 +0430

    Add size prop for Skills component

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -2,29 +2,59 @@ import { FC } from "react";
 
 type Props = {
   list: string[];
+  size?: string;
 };
 
-const Skills: FC<Props> = ({ list }) => (
-  <div>
-    {list.map((item: string, index: number) => (
-      <span
-        key={index}
-        style={{
-          color: "#333",
-          fontSize: "0.9em",
-          fontWeight: "bold",
-          marginRight: "0.5em",
-          marginBottom: "0.5em",
-          padding: ".05em 0.5em",
-          display: "inline-block",
-          border: "2px solid #333",
-          verticalAlign: "middle",
-        }}
-      >
-        {item}
-      </span>
-    ))}
-  </div>
-);
+const Skills: FC<Props> = ({ list, size }) => {
+  let fontSize = "0.9em";
+  let border = "2px solid #333";
+  let padding = ".05em 0.5em";
+  let marginRight = "0.5em";
+  let fontWeight = "bold";
+
+  switch (size) {
+    case "sm":
+      fontSize = "0.7em";
+      padding = "0";
+      border = "";
+      marginRight = "0.7em";
+      fontWeight = "normal";
+      break;
+
+    case "lg":
+      fontSize = "0.9em";
+      border = "2px solid #333";
+      padding = ".05em 0.5em";
+      marginRight = "0.5em";
+      fontWeight = "bold";
+      break;
+
+    default:
+      break;
+  }
+
+  return (
+    <div>
+      {list.map((item: string, index: number) => (
+        <span
+          key={index}
+          style={{
+            color: "#333",
+            fontSize: fontSize,
+            fontWeight: "bold",
+            marginRight: marginRight,
+            marginBottom: "0.5em",
+            padding: padding,
+            display: "inline-block",
+            border: border,
+            verticalAlign: "middle",
+          }}
+        >
+          {item}
+        </span>
+      ))}
+    </div>
+  );
+};
 
 export default Skills;
