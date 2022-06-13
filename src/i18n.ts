28efa4b11bddd2aed3153dfc5266e78e274ec220
MehdiKhoshnevisz <mkhnsz1996@gmail.com>
2022-06-13 12:19:45 +0430

    Add i18n config file

---
+++ src/i18n.ts
@@ -0,0 +1,20 @@
+import i18n from "i18next";
+import Backend from "i18next-http-backend";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+
+i18n
+  .use(Backend)
+  .use(initReactI18next)
+  .use(LanguageDetector)
+  .init({
+    fallbackLng: "en",
+    interpolation: {
+      escapeValue: false,
+    },
+    react: {
+      useSuspense: false,
+    },
+  });
+
+export default i18n;
