85cede50c1545e3c2ffbbe0391dd2b6eecfc06e1
MehdiKhoshnevisz <mkhnsz1996@gmail.com>
2022-06-13 14:06:51 +0430

    Update App with translations

--- src/App.tsx
+++ src/App.tsx
@@ -1,167 +1,72 @@
+import { useTranslation } from "react-i18next";
+
 import "./App.css";
-import Links from "./components/Links";
 import Footer from "./components/Footer";
 import Skills from "./components/Skills";
+import Socials from "./components/Socials";
 import Project from "./components/Project";
 import Contact from "./components/Contact";
 import Biography from "./components/Biography";
 import Exprience from "./components/Exprience";
 import Education from "./components/Education";
-import Achievement from "./components/Achievement";
 import ItemWrapper from "./components/ItemWrapper";
 import LineSeparator from "./components/LineSeparator";
 
 function App() {
+  const { t, ready } = useTranslation();
+  const skills: string[] = t("skills", { returnObjects: true });
+  const experiences: object[] = t("experiences", { returnObjects: true });
+  const education: object[] = t("education", { returnObjects: true });
+  const projects: object[] = t("projects", { returnObjects: true });
+  const socials: object[] = t("socials", { returnObjects: true });
+
+  if (!ready) return <></>;
+
   return (
     <>
       <main id="app">
-        <Biography
-          firstName="Mehdi"
-          lastName="Khoshnevis"
-          title="Senior Software Developer"
-          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque voluptate
-          repellat ipsam repudiandae explicabo ratione aliquid, quo maxime ab unde
-          distinctio nihil quod iure at ad, tempora odit mollitia. Amet repellat
-          ipsam repudiandae explicabo ratione aliquid, quo maxime ab unde
-          distinctio nihil quod iure at ad, tempora odit mollitia. Amet."
-        />
+        <Biography {...t("bio", { returnObjects: true })} />
 
         <LineSeparator />
 
         <ItemWrapper title="Contact">
-          <Contact
-            phone="+98 910 965 7619"
-            email="mkhnsz1996@gmail.com"
-            website="https://imehdi.dev"
-            location="Iran"
-          />
+          <Contact {...t("contact", { returnObjects: true })} />
         </ItemWrapper>
 
         <LineSeparator />
 
         <ItemWrapper title="Skills">
-          <Skills
-            list={["HTML5", "CSS3", "Javascript", "Vuejs", "Reactjs", "Nodejs"]}
-          />
+          <Skills list={skills} />
         </ItemWrapper>
 
         <LineSeparator />
 
         <ItemWrapper title="Work Exprience">
-          <Exprience
-            company="Apple Company"
-            type="Full-Time"
-            position="Technical lead"
-            startDate="2021"
-            website="https://apple.com"
-            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam euismod, nisi vitae tincidunt congue, nisl nunc egestas nisi, euismod aliquet nisl nunc eget."
-            skills={["HTML5", "CSS3", "Javascript", "Vuejs", "Nodejs"]}
-          />
-
-          <Exprience
-            company="Google"
-            type="Full-Time"
-            position="Senior Software Developer"
-            startDate="2018"
-            endDate="2020"
-            website="https://google.com"
-            description="Started and scaled design team to 6 product designers and Implemented a web application desgin System"
-            skills={["HTML5", "CSS3", "Javascript"]}
-          />
-
-          <Exprience
-            company="Buzzjective Company"
-            type="Contract"
-            position="Software Developer"
-            startDate="2016"
-            endDate="2018"
-            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam euismod, nisi vitae tincidunf."
-          />
+          {experiences.map((item: any) => {
+            return <Exprience {...item} />;
+          })}
         </ItemWrapper>
 
         <LineSeparator />
 
         <ItemWrapper title="Education">
-          <Education
-            title="Sharif University"
-            field="Computer Science &nbsp; - &nbsp; Software Engineering"
-            startDate="2018"
-            endDate="2022"
-          />
-
-          <Education
-            title="Shahab Danesh University"
-            field="Computer Engineer &nbsp; - &nbsp; Information Technology"
-            startDate="2014"
-            endDate="2018"
-          />
+          {education.map((item: any) => {
+            return <Education {...item} />;
+          })}
         </ItemWrapper>
 
         <LineSeparator />
 
         <ItemWrapper title="Projects">
-          <Project
-            title="vue-splash"
-            type="Personal"
-            url="https://github.com/MehdiKhoshnevisz/vue-splash"
-            startDate="2020"
-            endDate="2022"
-            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam euismod, nisi vitae tincidunt congue, nisl nunc egestas nisi, euismod aliquet nisl nunc eget."
-          />
-
-          <Project
-            title="simple-resume-builder"
-            type="Freelance"
-            url="https://github.com/MehdiKhoshnevisz/simple-resume-builder"
-            startDate="2020"
-            endDate="2022"
-            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam euismod, nisi vitae tincidunt congue, nisl nunc egestas nisi, euismod aliquet nisl nunc eget."
-          />
+          {projects.map((item: any) => {
+            return <Project {...item} />;
+          })}
         </ItemWrapper>
 
         <LineSeparator />
 
-        <ItemWrapper title="Achievement">
-          <Achievement description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam euismod, nisi vitae tincidunt congue, nisl nunc egestas nisi, euismod aliquet nisl nunc eget." />
-        </ItemWrapper>
-
-        <LineSeparator />
-
-        <ItemWrapper title="Links">
-          <Links
-            items={[
-              { title: "Telegram", url: "https://t.me/MehdiKhoshnevisz" },
-              {
-                title: "Instagram",
-                url: "https://instagram.com/mehdi_khoshnevisz",
-              },
-              {
-                title: "LinkedIn",
-                url: "https://linkedin.com/in/mehdi-khoshnevisz",
-              },
-              {
-                title: "Github",
-                url: "https://github.com/MehdiKhoshnevisz",
-              },
-              {
-                title: "Twitter",
-                url: "https://twitter.com/imehdi96",
-              },
-
-              {
-                title: "Stackoverflow",
-                url: "https://stackoverflow.com/users/9087806/mehdi-khoshnevisz",
-              },
-              {
-                title: "Unsplash",
-                url: "https://unsplash.com/@mehdi_khoshnevisz",
-              },
-              {
-                title: "Virgool",
-                url: "https://virgool.io/@mehdi_khoshnevisz",
-              },
-            ]}
-          />
+        <ItemWrapper title="Socials">
+          <Socials list={socials} />
         </ItemWrapper>
       </main>
 
