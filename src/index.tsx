6410d5e2d10d70618a314bb93b5cf554a769d825
MehdiKhoshnevisz <mkhnsz1996@gmail.com>
2022-06-16 22:48:30 +0430

    Fix ts typing error

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
-import { createRoot } from "react-dom/client";
-
 import "./i18n";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+
+const createRoot = require("react-dom/client").createRoot;
 
 const container = document.getElementById("root");
 const root = createRoot(container!);
