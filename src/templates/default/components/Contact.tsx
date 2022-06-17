4ea3e4d168085abd789028c0c5d1741fddf15ed3
MehdiKhoshnevisz <mkhnsz1996@gmail.com>
2022-06-17 15:45:02 +0430

    Fix Contact props empty state

--- src/templates/default/components/Contact.tsx
+++ src/templates/default/components/Contact.tsx
@@ -11,31 +11,39 @@ type Props = {
 
 const Contact: FC<Props> = ({ phone, email, website, location }) => (
   <>
-    <div>
-      <h3 className="mk-general-item-title">Phone</h3>
-      <a href={`tel:${phone}`} className="mk-general-item-sub-title">
-        {phone}
-      </a>
-    </div>
+    {phone && (
+      <div>
+        <h3 className="mk-general-item-title">Phone</h3>
+        <a href={`tel:${phone}`} className="mk-general-item-sub-title">
+          {phone}
+        </a>
+      </div>
+    )}
 
-    <div>
-      <h3 className="mk-general-item-title">Email</h3>
-      <a href={`mailto:${email}`} className="mk-general-item-sub-title">
-        {email}
-      </a>
-    </div>
+    {email && (
+      <div>
+        <h3 className="mk-general-item-title">Email</h3>
+        <a href={`mailto:${email}`} className="mk-general-item-sub-title">
+          {email}
+        </a>
+      </div>
+    )}
 
-    <div>
-      <h3 className="mk-general-item-title">Website</h3>
-      <a href={website} className="mk-general-item-sub-title">
-        {cleanURL(website)}
-      </a>
-    </div>
+    {website && (
+      <div>
+        <h3 className="mk-general-item-title">Website</h3>
+        <a href={website} className="mk-general-item-sub-title">
+          {cleanURL(website)}
+        </a>
+      </div>
+    )}
 
-    <div>
-      <h3 className="mk-general-item-title">Location</h3>
-      <span className="mk-general-item-sub-title">{location}</span>
-    </div>
+    {location && (
+      <div>
+        <h3 className="mk-general-item-title">Location</h3>
+        <span className="mk-general-item-sub-title">{location}</span>
+      </div>
+    )}
   </>
 );
 
