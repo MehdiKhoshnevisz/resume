31dd263ad8383315b1fc3dbfb1152e0fb242f6dc
MehdiKhoshnevisz <mkhnsz1996@gmail.com>
2022-06-17 16:56:13 +0430

    Remove extra use in i18n config

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,12 +1,10 @@
 import i18n from "i18next";
 import Backend from "i18next-http-backend";
 import { initReactI18next } from "react-i18next";
-import LanguageDetector from "i18next-browser-languagedetector";
 
 i18n
   .use(Backend)
   .use(initReactI18next)
-  .use(LanguageDetector)
   .init({
     backend: {
       loadPath: "locales/{{lng}}/{{ns}}.json",
@@ -18,6 +16,7 @@ i18n
     react: {
       useSuspense: false,
     },
+    keySeparator: false,
   });
 
 export default i18n;
