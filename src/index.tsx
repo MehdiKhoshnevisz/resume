c8b22666f47c90aebcc09cac4459d58feb23cba0
MehdiKhoshnevisz <mkhnsz1996@gmail.com>
2022-06-23 23:44:20 +0430

    Remove extra webVitals function

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,9 @@
 import "./i18n";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 
 const createRoot = require("react-dom/client").createRoot;
 
 const container = document.getElementById("root");
 const root = createRoot(container!);
 root.render(<App />);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
