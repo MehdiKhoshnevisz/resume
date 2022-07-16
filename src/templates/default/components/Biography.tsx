a134426c786923deb113a14206cb44c42b3e9691
MehdiKhoshnevisz <mkhnsz1996@gmail.com>
2022-07-16 07:28:34 +0430

    Add avatar in Biogaphy

--- src/templates/default/components/Biography.tsx
+++ src/templates/default/components/Biography.tsx
@@ -5,37 +5,82 @@ type Props = {
   lastName?: string;
   jobTitle: string;
   about: string;
+  avatar?: string;
 };
 
-const Biography: FC<Props> = ({ firstName, lastName, jobTitle, about }) => (
-  <header>
-    <h1
-      style={{
-        fontSize: "3em",
-        fontWeight: "900",
-        marginTop: 0,
-        marginBottom: ".1em",
-        lineHeight: 1,
-        color: "#333",
-        transform: "translateX(-2px)",
-      }}
-    >
-      {firstName} <br /> {lastName || ""}
-    </h1>
-    <span style={{ display: "block", fontSize: "1.3em", marginBottom: ".1em" }}>
-      {jobTitle}
-    </span>
-    <p
-      style={{
-        fontSize: "1em",
-        lineHeight: 1.8,
-        color: "#777",
-        margin: 0,
-      }}
-    >
-      {about}
-    </p>
-  </header>
-);
+const Biography: FC<Props> = ({
+  firstName,
+  lastName,
+  jobTitle,
+  about,
+  avatar,
+}) => {
+  const fullName = `${firstName} ${lastName || ""}`;
+  const fullNameNewLine = `${firstName} <br /> ${lastName || ""}`;
+
+  return (
+    <header>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "flex-end",
+        }}
+      >
+        {avatar && (
+          <div
+            style={{
+              padding: "0.4em",
+              marginRight: "1em",
+              border: "1px solid",
+              marginBottom: "0.5rem",
+            }}
+          >
+            <img
+              src={avatar}
+              style={{
+                width: "135px",
+                height: "135px",
+                objectFit: "cover",
+                verticalAlign: "middle",
+                objectPosition: "center",
+                filter: "grayscale(100%)",
+              }}
+              alt={fullName}
+            />
+          </div>
+        )}
+
+        <div>
+          <h1
+            style={{
+              fontSize: "3em",
+              fontWeight: "900",
+              marginTop: 0,
+              marginBottom: ".1em",
+              lineHeight: 1,
+              color: "#333",
+              transform: "translateX(-2px)",
+            }}
+            dangerouslySetInnerHTML={{ __html: fullNameNewLine }}
+          />
+          <span style={{ display: "block", fontSize: "1.3em" }}>
+            {jobTitle}
+          </span>
+        </div>
+      </div>
+
+      <p
+        style={{
+          fontSize: "1em",
+          lineHeight: 1.8,
+          color: "#777",
+          margin: 0,
+        }}
+      >
+        {about}
+      </p>
+    </header>
+  );
+};
 
 export default Biography;
