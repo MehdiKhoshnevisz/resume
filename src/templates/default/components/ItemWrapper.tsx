53088de31ab0f32e7ff5f890eec1720310d159a7
MehdiKhoshnevisz <mkhnsz1996@gmail.com>
2022-07-17 11:34:32 +0430

    Add css class for item warpper title

--- src/templates/default/components/ItemWrapper.tsx
+++ src/templates/default/components/ItemWrapper.tsx
@@ -6,18 +6,8 @@ type Props = {
 };
 
 const ItemWrapper: FC<Props> = ({ title, children }) => (
-  <div className="mk-resume-data__item">
-    <h2
-      style={{
-        fontSize: "1.3em",
-        fontWeight: "900",
-        color: "#333",
-        margin: 0,
-        marginBottom: ".5em",
-      }}
-    >
-      {title}
-    </h2>
+  <div className="mk-item-wrapper">
+    <h2 className="mk-item-wrapper__title">{title}</h2>
     <div className="mk-item-wrapper__children">{children || ""}</div>
   </div>
 );
