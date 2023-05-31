618709491747a73564dd9f8ac3472b0497a17bd0
MehdiKhoshnevisz <mkhnsz1996@gmail.com>
2023-05-31 20:27:18 +0330

    Add row prop for ItemWrapper component

--- src/templates/default/components/ItemWrapper.tsx
+++ src/templates/default/components/ItemWrapper.tsx
@@ -2,14 +2,34 @@ import { FC, ReactNode } from "react";
 
 type Props = {
   title?: string;
-  row?: boolean;
+  row?: string;
   children: ReactNode | ReactNode[];
 };
 
-const ItemWrapper: FC<Props> = ({ title, children, row  }) => (
-  <div className="mk-item-wrapper">
-    <h2 className="mk-item-wrapper__title">{title}</h2>
-    <div className={`${row ? 'mk-item-wrapper__children--row' : "mk-item-wrapper__children"}`}>{children || ""}</div>
-  </div>
-);
+const ItemWrapper: FC<Props> = ({ title, children, row  }) => {
+  const childrenClass = () => {
+    let className = 'mk-item-wrapper__children';
+
+    if (!row) return className;
+
+    switch (row) {
+      case 'between':
+        className = 'mk-item-wrapper__children--row-between';
+        break;
+
+      case 'start':
+        className = 'mk-item-wrapper__children--row-start';
+        break;
+    }
+
+    return className
+  }
+
+  return (
+    <div className="mk-item-wrapper">
+      <h2 className="mk-item-wrapper__title">{title}</h2>
+      <div className={childrenClass()}>{children || ""}</div>
+    </div>
+  );
+}
 export default ItemWrapper;
