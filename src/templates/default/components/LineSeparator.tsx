75ad883ccc7c53380357d8494ac4c48a4a35433f
MehdiKhoshnevisz <mkhnsz1996@gmail.com>
2023-06-17 13:14:23 +0330

    Update libe separator style

--- src/templates/default/components/LineSeparator.tsx
+++ src/templates/default/components/LineSeparator.tsx
@@ -1,7 +1,14 @@
 import { FC } from "react";
 
 const LineSeparator: FC = () => (
-  <hr style={{ margin: "1.5em 0", opacity: 0.3 }} />
+  <hr
+    style={{
+      margin: "1.5em 0",
+      background: "#F5F5F5",
+      border: "none",
+      height: "1.5px",
+    }}
+  />
 );
 
 export default LineSeparator;
